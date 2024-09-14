ff0ca5b1cee58a58924c7bbda7c13eb8b1899630
notch-devOS <gustavonunes.pereira@icloud.com>
2024-09-14 12:25:44 -0300

    Finish Header UI Design

--- Prioritize/ContentView.swift
+++ Prioritize/ContentView.swift
@@ -6,56 +6,99 @@
 //
 
 import SwiftUI
-import SwiftData
+import Contacts
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        VStack {
+            HStack {
+                // Esse componente representa o avatar do usuário
+                Avatar()
+                Spacer()
+                    .frame(width: 15)
+                // Esse componente representa a saudação ao usuário
+                VStack(alignment: .leading) {
+                    Text("Hello,")
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundStyle(.gray)
+                    Text("Gustavo Nunes")
+                        .font(.system(size: 20, weight: .medium))
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                Spacer()
+                // Esse componente representa um botão
+                Button(action: {
+                    print("Botão de calendário pressionado")
+                }) {
+                    Image(systemName: "calendar")
+                        .foregroundStyle(.black)
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray))
+                Button(action: {
+                    print("Botão de calendário pressionado")
+                }) {
+                    Image(systemName: "bell")
+                        .foregroundStyle(.black)
                 }
-            }
-        } detail: {
-            Text("Select an item")
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray))
+            }.padding()
+            Spacer()
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
+   
+}
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+struct Avatar: View {
+    
+    @State private var avatar: UIImage? = nil
+        
+    var body: some View {
+        
+        if let avatar = avatar {
+            Image(uiImage: avatar)
+                .resizable()
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+        } else {
+            Image(systemName: "person")
+                .font(.system(size: 30))
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+                .onAppear { fetchUserAvatar() }
+        }
+        
+        
+        
+    }
+    
+    func fetchUserAvatar() {
+        
+        let store = CNContactStore()
+        let keys = [CNContactThumbnailImageDataKey] as [CNKeyDescriptor]
+        
+        do {
+            let containers = try store.containers(matching: nil)
+            for container in containers {
+                let predicate = CNContact.predicateForContactsInContainer(withIdentifier: container.identifier)
+                let contacts = try store.unifiedContacts(matching: predicate, keysToFetch: keys)
+                
+                if let contact = contacts.first, let imageData = contact.thumbnailImageData {
+                    avatar = UIImage(data: imageData)
+                }
             }
+        } catch {
+            print("Error fetching user avatar: \(error)")
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        
 }
